f580a0869a4303616da42039848da8302607fc3d
AdishDevs <adishkanimsara.adikari@gmail.com>
2025-05-26 15:00:08 +0530

    Initial Commit

---
+++ FakeStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeStore
+//
+//  Created by Admin on 2025-05-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Admin on 2025-05-26.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
