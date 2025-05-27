54a44b1e9aa396d0d1be1260bd00356ec9a3dfbf
AdishDevs <adishkanimsara.adikari@gmail.com>
2025-05-27 10:55:29 +0530

    Updated Product details

---
+++ FakeStore/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  FakeStore
+//
+//  Created by Admin on 2025-05-26.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
